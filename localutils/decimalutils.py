0c1bd7ef5c1306f55b3ac72591ff1c41bb1318ef
Adam Hedges <achedges@gmail.com>
2019-05-08 09:25:55 -0500

    Added decimal truncation function and unit test

--- localutils/decimalutils.py
+++ localutils/decimalutils.py
@@ -6,3 +6,7 @@ def parse(decimalinput):
 		return ret
 	except:
 		return None
+
+def truncate(floatinput: float, precision: int = 2):
+	ret = int(floatinput * (10**precision))
+	return float(ret) / (10**precision)
--- tests/TestDecimalUtils.py
+++ tests/TestDecimalUtils.py
@@ -10,3 +10,10 @@ class Test(unittest.TestCase):
 		self.assertEqual(decimalutils.parse(1.0), 1.0)
 		self.assertEqual(decimalutils.parse(1.12345678), 1.1234)
 		return
+
+	def testTruncate(self):
+		self.assertEqual(decimalutils.truncate(123.456, precision=2), 123.45)
+		self.assertEqual(decimalutils.truncate(123, precision=2), 123.0)
+		self.assertEqual(decimalutils.truncate(123.4, precision=3), 123.4)
+		self.assertEqual(decimalutils.truncate(123.456789, precision=5), 123.45678)
+		return
