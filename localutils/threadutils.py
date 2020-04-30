6945ec2996f40e5d1d6c4312900a58610cc55584
Adam Hedges <achedges@gmail.com>
2020-04-30 09:09:04 -0500

    Rewrote threadutils

--- localutils/threadutils.py
+++ localutils/threadutils.py
@@ -1,13 +1,23 @@
+from enum import Enum
 from time import sleep
 
 
-class ThreadWait:
-	def __init__(self, resolution=.1, initialWait=1, increment=2):
-		self.resolution = resolution
-		self.waitval = initialWait
-		self.increment = increment # 1=linear, 2=exponential
+class ThreadWaitMultiplier(Enum):
+	Fixed = 1
+	Exponential = 2
 
-	def waitnext(self):
-		sleep(self.resolution * self.waitval)
-		self.waitval *= self.increment
+class ThreadWait:
+
+	def __init__(self, initialMilliseconds: float=100, multiplier: ThreadWaitMultiplier=ThreadWaitMultiplier.Exponential):
+		self.initialwait: float = initialMilliseconds / 1000 # sleep() operates on seconds
+		self.waitval: float = self.initialwait
+		self.multiplier: ThreadWaitMultiplier = multiplier
+
+	def waitnext(self) -> None:
+		sleep(self.waitval)
+		self.waitval *= self.multiplier.value
+		return
+
+	def reset(self) -> None:
+		self.waitval = self.initialwait
 		return
